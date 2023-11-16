63f1b3408c8ff1fdc837408a576b16cf060c2521
Luiz <l.guilherme128@gmail.com>
2023-11-16 18:53:46 -0300

    Praticando c#

---
+++ OOP/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle x, y;
+            x = new Triangle();
+            y = new Triangle();
+
+            System.Console.WriteLine("Entre com as medidas do triangulo X:");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            System.Console.WriteLine("Entre com as medidas do triangulo Y:");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = x.Area(true);
+            double areaY = y.Area(false);
+
+            System.Console.WriteLine("Area de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            System.Console.WriteLine("Area de y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+                System.Console.WriteLine("Maior area: X");
+            else
+                System.Console.WriteLine("Maior arear: Y");
+
+        }
+    }
+}
